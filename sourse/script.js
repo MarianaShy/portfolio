8ed059fbb0d75266279934e6bfda156966dce23d
Mariana Shydlovska <marjanka77777@gmail.com>
2024-01-30 01:18:00 +0100

    optimize toggleSide menu function

--- sourse/script.js
+++ sourse/script.js
@@ -3,18 +3,10 @@
 
 //navbar
 
-
-	const sideMenu = document.getElementById("my-nav-menu");
-	const burger = document.getElementById("burger");
-	const main = document.getElementById("main");
-
-
-	
-	function toggleSideMenu() {
+/*function toggleSideMenu() {
 		if(sideMenu.classList.contains('hidden')){
 				sideMenu.classList.remove('hidden')
 		}
-
 		else{
 			sideMenu.classList.add('hidden')
 			}
@@ -23,8 +15,15 @@
 		if(!sideMenu.classList.contains('hidden')){
 				sideMenu.classList.add('hidden')
 		}
-	}
-burger.addEventListener("click", toggleSideMenu) 
-main.addEventListener("click", closeSideMenu)
+	}*/
+
+
+	const sideMenu = document.getElementById("my-nav-menu");
+	const burger = document.getElementById("burger");
+	const main = document.getElementById("main");
+
+
+burger.addEventListener("click", ()=>{(sideMenu.classList.contains('hidden')) ? sideMenu.classList.remove('hidden'):  sideMenu.classList.add('hidden')});
+main.addEventListener("click", ()=>{ if (!sideMenu.classList.contains('hidden')) {sideMenu.classList.add('hidden')}});
 	
 
