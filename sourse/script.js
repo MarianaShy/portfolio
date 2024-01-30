caedd7c37af83796eea491ace5d0f188fddec633
Mariana Shydlovska <marjanka77777@gmail.com>
2024-01-30 01:07:14 +0100

    add toggle menu function

--- sourse/script.js
+++ sourse/script.js
@@ -1,4 +1,30 @@
 `use strict`
 
 
+//navbar
+
+
+	const sideMenu = document.getElementById("my-nav-menu");
+	const burger = document.getElementById("burger");
+	const main = document.getElementById("main");
+
+
+	
+	function toggleSideMenu() {
+		if(sideMenu.classList.contains('hidden')){
+				sideMenu.classList.remove('hidden')
+		}
+
+		else{
+			sideMenu.classList.add('hidden')
+			}
+	}
+	function closeSideMenu() {
+		if(!sideMenu.classList.contains('hidden')){
+				sideMenu.classList.add('hidden')
+		}
+	}
+burger.addEventListener("click", toggleSideMenu) 
+main.addEventListener("click", closeSideMenu)
+	
 
