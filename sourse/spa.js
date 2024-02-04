69f4718ab9fc81e0b8da0fa24abf4d3f0c2804d2
Mariana Shydlovska <marjanka77777@gmail.com>
2024-02-04 02:48:40 +0100

    Update spa.js

--- sourse/spa.js
+++ sourse/spa.js
@@ -1,1 +1,35 @@
+'use strict'
 
+
+
+const pages = {
+		'home' : `Welcome to XYZ Company. This is our home page!<br/><br/><img src='lion.jpg' style='width:500px;' />`,
+		'aboutus' : `Our About Us page<br/><br/><img src='lion2.jpg' style='width:500px;' />`,
+		'services' : `Our Services page<br/><br/><img src='lion3.jpg' style='width:500px;' />`,
+		'contact' : `Our Contact Us page<br/><br/><img src='lion4.jpg' style='width:500px;' />`
+	};
+
+	function getPageContent(page){
+		let contentToReturn;
+		switch(page){
+			case 'home':
+				contentToReturn = pages.home;
+				break;
+			case 'work':
+				contentToReturn = pages.work;
+				break;
+			case 'bio':
+				contentToReturn = pages.bio;
+				break;
+			case 'contacts':
+				contentToReturn = pages.contacts;
+				break;
+        case 'admin':
+				contentToReturn = pages.admin;
+				break;
+			default:
+				contentToReturn = pages.home;
+				break;				
+		}
+		document.getElementById('content').innerHTML = contentToReturn; 
+	}
